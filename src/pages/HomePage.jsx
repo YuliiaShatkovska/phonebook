73648dee28c08f157378b29d6334f4823f684ba7
Yuliia Shatkovska <kushnir.jul@yahoo.com>
2024-04-30 15:55:36 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,39 @@
 import { NavLink } from 'react-router-dom';
 import { Container } from 'components/App/App.styled';
 import { HomeBox, HomeText, HomeTitle, HomeWrap } from './HomePage.styled';
+import { useSelector } from 'react-redux';
+import { selectIsLoading } from '../redux/contacts/selectors';
 
 const HomePage = () => {
+  const isLoggedIn = useSelector(selectIsLoading);
+
   return (
     <HomeWrap>
       <Container>
         <HomeBox>
           <HomeTitle>CONTACTS</HomeTitle>
 
-          <HomeText>
-            To use this app you need to{' '}
-            <NavLink to="login" style={{ color: '#61b3b5' }}>
-              Log in
-            </NavLink>{' '}
-            or{' '}
-            <NavLink to="register" style={{ color: '#61b3b5' }}>
-              Sign up
-            </NavLink>
-            !
-          </HomeText>
+          {isLoggedIn ? (
+            <HomeText>
+              Go to your{' '}
+              <NavLink to="contact" style={{ color: '#61b3b5' }}>
+                contact list
+              </NavLink>{' '}
+              !
+            </HomeText>
+          ) : (
+            <HomeText>
+              To use this app you need to{' '}
+              <NavLink to="login" style={{ color: '#61b3b5' }}>
+                Log in
+              </NavLink>{' '}
+              or{' '}
+              <NavLink to="register" style={{ color: '#61b3b5' }}>
+                Sign up
+              </NavLink>
+              !
+            </HomeText>
+          )}
         </HomeBox>
       </Container>
     </HomeWrap>
